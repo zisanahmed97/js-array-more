e05af8e3efb5abe29420495c6ad523369b81c295
zisanahmed97 <zisanahmed97@gmail.com>
2026-01-30 18:43:32 +0600

    created js-array-more folder and added loopping file

---
+++ loopping.js
@@ -0,0 +1,27 @@
+const friends = ['elon', 'mark', 'bill', 'waren'];
+for(const friend of friends){
+    // console.log(friend);
+}
+
+
+for(let i = 0; i <friends.length; i++){
+    // console.log(i);
+    // console.log(friends[i]);
+
+}
+
+let n = 0;
+while(n < friends.length){
+    // console.log(friends[n]);
+    n++;
+}
+
+
+const numbers = [23,45,56,2,4,67657,767,334,7,54,21,5];
+
+let num = 0;
+while(num <numbers.length){
+    console.log(numbers[num]);
+    num++;
+
+}
