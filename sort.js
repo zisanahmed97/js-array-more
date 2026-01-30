63f12c20e3c7d72e4109cc45235880e61e004705
zisanahmed97 <zisanahmed97@gmail.com>
2026-01-30 20:32:25 +0600

    added sort file

---
+++ sort.js
@@ -0,0 +1,11 @@
+const friends = ['rakib','nokib','akib','pokib','boikb'];
+// console.log(friends.sort());
+
+
+// ai sort() ta shob shomoy thik moto kaj kore na karon eta string wise kaj kore 
+
+//example:
+
+const numbers = [23,4,2,67,45,8];
+console.log(numbers.sort()); // eta sort korbe na karon 2 to asei abar 2 diye shuru 23 o ase 
+
